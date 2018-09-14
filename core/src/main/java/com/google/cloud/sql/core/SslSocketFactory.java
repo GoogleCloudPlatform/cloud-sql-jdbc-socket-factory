cd0c04ba77c1face924e9452ae1718b4b236c298
Hsiu-Fan Wang <hfwang@porkbuns.net>
2018-09-13 17:24:44 -0700

    Move checking logic into getPreferredIp.

--- core/src/main/java/com/google/cloud/sql/core/SslSocketFactory.java
+++ core/src/main/java/com/google/cloud/sql/core/SslSocketFactory.java
@@ -193,19 +193,6 @@ public class SslSocketFactory {
       throws IOException {
     InstanceSslInfo instanceSslInfo = getInstanceSslInfo(instanceName, certificateCaching);
     String ipAddress = getPreferredIp(ipTypes, instanceSslInfo);
-    if (ipAddress == null) {
-      StringBuilder sb = new StringBuilder();
-      for (String type : ipTypes) {
-        if (sb.length() > 0) {
-          sb.append(", ");
-        }
-        sb.append(type.toUpperCase());
-      }
-      throw new RuntimeException(
-          String.format(
-              "Cloud SQL instance [%s] does not have any IP addresses matching preference: [ %s ]",
-              instanceName, sb));
-    }
 
     logger.info(
         String.format(
@@ -249,6 +236,21 @@ public class SslSocketFactory {
         break;
       }
     }
+
+    if (ipAddress == null) {
+      StringBuilder sb = new StringBuilder();
+      for (String type : ipTypes) {
+        if (sb.length() > 0) {
+          sb.append(", ");
+        }
+        sb.append(type.toUpperCase());
+      }
+      throw new RuntimeException(
+          String.format(
+              "Cloud SQL instance [%s] does not have any IP addresses matching preference: [ %s ]",
+              instanceName, sb));
+    }
+
     return ipAddress;
   }
 
