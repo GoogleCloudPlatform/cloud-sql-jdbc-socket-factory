67740b0fc8190654aae23fc3d5b10cd30977913c
Kurtis Van Gent <kvg@google.com>
2019-05-07 10:59:54 -0700

    Add updateInstanceMetadata for fetching InstanceMetadata.

--- core/src/main/java/com/google/cloud/sql/core/CloudSqlInstance.java
+++ core/src/main/java/com/google/cloud/sql/core/CloudSqlInstance.java
@@ -1,6 +1,12 @@
 package com.google.cloud.sql.core;
 
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 import com.google.api.services.sqladmin.SQLAdmin;
+import com.google.api.services.sqladmin.model.DatabaseInstance;
+import com.google.api.services.sqladmin.model.IpMapping;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /** CloudSqlInstance is used for retrieving and referencing info related to a Cloud SQL instance. */
 class CloudSqlInstance {
@@ -8,6 +14,7 @@ class CloudSqlInstance {
   private final String projectId;
   private final String regionId;
   private final String instanceId;
+  private Map<String, String> ipAddrs = new HashMap<>();
 
   CloudSqlInstance(String connectionName, SQLAdmin apiClient) {
     String[] connFields = connectionName.split(":");
@@ -20,5 +27,80 @@ class CloudSqlInstance {
     this.projectId = connFields[0];
     this.regionId = connFields[1];
     this.instanceId = connFields[2];
+    this.updateInstanceMetadata(apiClient);
+  }
+
+  /**
+   * updateInstanceMetadata uses the Cloud SQL Admin API to fetch Instance Info.
+   *
+   * @param apiClient {@link SQLAdmin} client used to make the requests needed.
+   */
+  private void updateInstanceMetadata(SQLAdmin apiClient) {
+    DatabaseInstance instanceMetadata;
+    try {
+      instanceMetadata = apiClient.instances().get(projectId, instanceId).execute();
+    } catch (IOException ex) {
+      throw addExceptionContext(
+          ex,
+          String.format("[%s] Failed to refresh metadata for Cloud SQL instance.", connectionName));
+    }
+
+    if (!instanceMetadata.getBackendType().equals("SECOND_GEN")) {
+      throw new IllegalArgumentException(
+          "[%s] Connections to Cloud SQL instance not supported - not a Second Generation "
+              + "instance.");
+    }
+    if (!instanceMetadata.getRegion().equals(regionId)) {
+      throw new IllegalArgumentException(
+          "[%s] The region specified for the Cloud SQL instance is"
+              + " incorrect. Please verify the instance connection name.");
+    }
+    synchronized (this) {
+      for (IpMapping addr : instanceMetadata.getIpAddresses()) {
+        ipAddrs.put(addr.getType(), addr.getIpAddress());
+      }
+
+      if (ipAddrs.isEmpty()) {
+        throw new IllegalStateException(
+            "[%s] Unable to connect to Cloud SQL instance: instance "
+                + "does not have an assigned IP address.");
+      }
+    }
+  }
+
+  // createApiExceptions checks for common api errors and adds additional context.
+  private RuntimeException addExceptionContext(IOException ex, String fallbackDesc) {
+    // Verify we are able to extract a reason from an exception, or fallback to a generic desc
+    GoogleJsonResponseException gjrEx =
+        ex instanceof GoogleJsonResponseException ? (GoogleJsonResponseException) ex : null;
+    if (gjrEx == null
+        || gjrEx.getDetails() == null
+        || gjrEx.getDetails().getErrors() == null
+        || gjrEx.getDetails().getErrors().isEmpty()) {
+      return new RuntimeException(fallbackDesc, ex);
+    }
+    // Check for commonly occurring user errors and add additional context
+    String reason = gjrEx.getDetails().getErrors().get(0).getReason();
+    if ("accessNotConfigured".equals(reason)) {
+      // This error occurs when the project doesn't have the "Cloud SQL Admin API" enabled
+      String apiLink =
+          "https://console.cloud.google.com/apis/api/sqladmin/overview?project=" + projectId;
+      return new RuntimeException(
+          String.format(
+              "[%s] The Google Cloud SQL Admin API is not enabled for the project \"%s\". Please "
+                  + "use the Google Developers Console to enable it: %s",
+              connectionName, projectId, apiLink));
+    } else if ("notAuthorized".equals(reason)) {
+      // This error occurs if the instance doesn't exist or the account isn't authorized
+      // TODO(kvg): Add credential account name to error string.
+      return new RuntimeException(
+          String.format(
+              "[%s] The Cloud SQL Instance does not exist or your account is not authorized to "
+                  + "access it. Please verify the instance connection name and check the IAM "
+                  + "permissions for project \"%s\" ",
+              connectionName, projectId));
+    }
+    // Fallback to the generic description
+    return new RuntimeException(fallbackDesc, ex);
   }
 }
--- core/src/test/java/com/google/cloud/sql/core/CloudSqlInstanceTest.java
+++ core/src/test/java/com/google/cloud/sql/core/CloudSqlInstanceTest.java
@@ -1,27 +1,69 @@
 package com.google.cloud.sql.core;
 
 import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.fail;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
 import com.google.api.services.sqladmin.SQLAdmin;
+import com.google.api.services.sqladmin.model.DatabaseInstance;
+import com.google.api.services.sqladmin.model.IpMapping;
+import com.google.common.collect.ImmutableList;
+import java.io.IOException;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 @RunWith(JUnit4.class)
 public class CloudSqlInstanceTest {
 
+  private static final String PUBLIC_IP = "127.0.0.1";
+
   @Mock private SQLAdmin sqlAdminClient;
+  @Mock private SQLAdmin.Instances sqlAdminClientInstances;
+  @Mock private SQLAdmin.Instances.Get sqlAdminClientInstancesGet;
+
+  @Before
+  public void setup() throws IOException {
+    MockitoAnnotations.initMocks(this);
+
+    when(sqlAdminClient.instances()).thenReturn(sqlAdminClientInstances);
+    when(sqlAdminClientInstances.get(anyString(), anyString()))
+        .thenReturn(sqlAdminClientInstancesGet);
+  }
 
   @Test
-  public void throwsExceptionForInvalidConnectionName() {
+  public void create_throwsExceptionForInvalidConnectionName() {
     String[] tests = new String[] {"foo", "foo:bar"};
     for (String test : tests) {
       try {
         CloudSqlInstance instance = new CloudSqlInstance(test, sqlAdminClient);
+        fail();
       } catch (IllegalArgumentException ex) {
         assertThat(ex.getMessage()).contains("Cloud SQL connection name is invalid");
       }
+    }
+  }
+
+  @Test
+  public void create_throwsErrorForInvalidInstanceRegion() throws IOException {
+    DatabaseInstance mockMetadata =
+        new DatabaseInstance()
+            .setBackendType("SECOND_GEN")
+            .setIpAddresses(ImmutableList.of(new IpMapping().setIpAddress(PUBLIC_IP)))
+            .setRegion("not_bar");
+    when(sqlAdminClient.instances().get(anyString(), anyString()).execute())
+        .thenReturn(mockMetadata);
+
+    try {
+      CloudSqlInstance instance = new CloudSqlInstance("foo:bar:baz", sqlAdminClient);
+      fail();
+    } catch (IllegalArgumentException e) {
+      assertThat(e.getMessage())
+          .contains("The region specified for the Cloud SQL instance is incorrect");
     }
   }
 }
