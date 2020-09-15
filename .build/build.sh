6cb381a8a90df852fb3fea29336bd6ac9fded18c
Shubha Rajan <shubhadayini@google.com>
2020-09-15 13:22:33 -0700

    Update .build/build.sh
    Co-authored-by: Kurtis Van Gent <31518063+kurtisvg@users.noreply.github.com>

--- .build/build.sh
+++ .build/build.sh
@@ -40,10 +40,15 @@ gsutil rm -f gs://$BUCKET_NAME/v"$VERSION"/*.json 2> /dev/null || true
 
 # Generate sha256 hashes for authentication
 echo -e "Add the following table to the release notes on GitHub: \n\n"
-echo "| filename | sha256 hash |"
-echo "|----------|-------------|"
-for f in $(gsutil ls "gs://$BUCKET_NAME/v$VERSION/*"); do
-    file=$(basename "$f")
-    sha=$(gsutil cat "$f" | sha256sum --binary)
-    echo "| [$file](https://storage.googleapis.com/$BUCKET_NAME/v$VERSION/$file) | $sha |"
+types=("connector-j postgres r2dbc-mysql r2dbc-postgres")
+for t in "${types[@]}"; do 
+    echo "### $t"
+    echo "| filename | sha256 hash |"
+    echo "|----------|-------------|"
+    for f in $(gsutil ls "gs://$BUCKET_NAME/v$VERSION/$t*"); do
+        file=$(basename "$f")
+        sha=$(gsutil cat "$f" | sha256sum --binary)
+        echo "| [$file](https://storage.googleapis.com/$BUCKET_NAME/v$VERSION/$file) | $sha |"
+    done
+    echo -e "\n\n"
 done
