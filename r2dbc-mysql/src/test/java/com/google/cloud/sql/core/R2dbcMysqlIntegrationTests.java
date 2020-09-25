3716d6dc97ad4d8fa55012b1055d9b8a55d25c2b
Shubha Rajan <shubhadayini@google.com>
2020-09-25 15:05:49 -0700

    test: added/updated e2e tests for all connectors (#301)
    * chore: added/updated e2e tests for all connectors
    
    * formatting
    
    * use UUID in table name
    
    * remove IF NOT EXISTS from table creation

--- r2dbc-mysql/src/test/java/com/google/cloud/sql/core/R2dbcMysqlIntegrationTests.java
+++ r2dbc-mysql/src/test/java/com/google/cloud/sql/core/R2dbcMysqlIntegrationTests.java
@@ -16,15 +16,20 @@
 
 package com.google.cloud.sql.core;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.common.truth.Truth.assertWithMessage;
 
+import com.google.common.collect.ImmutableList;
+import io.r2dbc.pool.ConnectionPool;
+import io.r2dbc.pool.ConnectionPoolConfiguration;
 import io.r2dbc.spi.ConnectionFactories;
 import io.r2dbc.spi.ConnectionFactory;
 import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.Timeout;
@@ -32,46 +37,86 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import reactor.core.publisher.Mono;
 
-@Ignore
 @RunWith(JUnit4.class)
 public class R2dbcMysqlIntegrationTests {
 
-  @Rule
-  public Timeout globalTimeout= new Timeout(20, TimeUnit.SECONDS);
+  private static ImmutableList<String> requiredEnvVars = ImmutableList
+      .of("MYSQL_USER", "MYSQL_PASS", "MYSQL_DB", "MYSQL_CONNECTION_NAME");
 
-  private ConnectionFactory connectionFactory;
+  private static final String CONNECTION_NAME = System.getenv("MYSQL_CONNECTION_NAME");
+  private static final String DB_NAME = System.getenv("MYSQL_DB");
+  private static final String DB_USER = System.getenv("MYSQL_USER");
+  private static final String DB_PASSWORD = System.getenv("MYSQL_PASS");
+
+  @Rule
+  public Timeout globalTimeout = new Timeout(20, TimeUnit.SECONDS);
+
+  private ConnectionPool connectionPool;
+  private String tableName;
+
+
+  @BeforeClass
+  public static void checkEnvVars() {
+    // Check that required env vars are set
+    requiredEnvVars.stream().forEach((varName) -> {
+      assertWithMessage(
+          String.format("Environment variable '%s' must be set to perform these tests.", varName))
+          .that(System.getenv(varName)).isNotEmpty();
+    });
+  }
 
   @Before
-  public void setUpConnection() {
-    this.connectionFactory =
-        ConnectionFactories.get(
-            "r2dbc:gcp:mysql://user:password@connectionString/dbName");
+  public void setUpPool() {
+    // Check that required env vars are set
+    this.requiredEnvVars.stream().forEach((varName) -> {
+      assertWithMessage(
+          String.format("Environment variable '%s' must be set to perform these tests.", varName))
+          .that(System.getenv(varName)).isNotEmpty();
+    });
 
-    Mono.from(this.connectionFactory.create())
+    // Set up URL parameters
+    String r2dbcURL = String
+        .format("r2dbc:gcp:mysql://%s:%s@%s/%s", DB_USER, DB_PASSWORD, CONNECTION_NAME,
+            DB_NAME);
+
+    // Initialize connection pool
+    ConnectionFactory connectionFactory = ConnectionFactories.get(r2dbcURL);
+    ConnectionPoolConfiguration configuration = ConnectionPoolConfiguration
+        .builder(connectionFactory)
+        .build();
+
+    this.connectionPool = new ConnectionPool(configuration);
+    this.tableName = String.format("books_%s", UUID.randomUUID().toString().replace("-", ""));
+
+    // Create table
+    Mono.from(this.connectionPool.create())
         .flatMapMany(
             c ->
                 c.createStatement(
-                        "CREATE TABLE IF NOT EXISTS BOOKS ("
-                            + "  ID CHAR(20) NOT NULL,"
-                            + "  TITLE TEXT NOT NULL"
-                            + ")")
+                    String.format("CREATE TABLE %s (", this.tableName)
+                        + "  ID CHAR(20) NOT NULL,"
+                        + "  TITLE TEXT NOT NULL"
+                        + ")")
                     .execute())
         .blockLast();
   }
 
+
   @After
   public void dropTableIfPresent() {
-    Mono.from(this.connectionFactory.create())
-        .delayUntil(c -> c.createStatement("DROP TABLE BOOKS").execute())
+    String dropStmt = String.format("DROP TABLE %s", this.tableName);
+    Mono.from(this.connectionPool.create())
+        .delayUntil(c -> c.createStatement(dropStmt).execute())
         .block();
   }
 
   @Test
-  public void insertTest() {
-    Mono.from(this.connectionFactory.create())
+  public void pooledConnectionTest() {
+    String insertStmt = String.format("INSERT INTO %s (ID, TITLE) VALUES (?, ?)", this.tableName);
+    Mono.from(this.connectionPool.create())
         .flatMapMany(
             c ->
-                c.createStatement("INSERT BOOKS (ID, TITLE) VALUES (?, ?)")
+                c.createStatement(insertStmt)
                     .bind(0, "book1")
                     .bind(1, "Book One")
                     .add()
@@ -81,14 +126,15 @@ public class R2dbcMysqlIntegrationTests {
         .flatMap(result -> result.map((row, rowMetadata) -> row.get(0)))
         .blockLast();
 
+    String selectStmt = String.format("SELECT TITLE FROM %s ORDER BY ID", this.tableName);
     List<String> books =
-        Mono.from(this.connectionFactory.create())
+        Mono.from(this.connectionPool.create())
             .flatMapMany(
                 connection ->
-                    connection.createStatement("SELECT TITLE FROM BOOKS ORDER BY ID").execute())
+                    connection.createStatement(selectStmt).execute())
             .flatMap(
-                spannerResult ->
-                    spannerResult.map(
+                result ->
+                    result.map(
                         (r, meta) -> r.get("TITLE", String.class)))
             .collectList()
             .block();
