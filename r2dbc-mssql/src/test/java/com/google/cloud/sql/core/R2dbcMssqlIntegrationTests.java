4ffa943c9ba5d70c28651669e7777dd7f99ba9c8
dsolomakha <dsolomakha@google.com>
2020-10-26 19:34:03 -0400

    PR comments

--- r2dbc-mssql/src/test/java/com/google/cloud/sql/core/R2dbcMssqlIntegrationTests.java
+++ r2dbc-mssql/src/test/java/com/google/cloud/sql/core/R2dbcMssqlIntegrationTests.java
@@ -16,15 +16,20 @@
 
 package com.google.cloud.sql.core;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.common.truth.Truth.assertWithMessage;
 
+import com.google.common.collect.ImmutableList;
+import io.r2dbc.pool.ConnectionPool;
+import io.r2dbc.pool.ConnectionPoolConfiguration;
 import io.r2dbc.spi.ConnectionFactories;
 import io.r2dbc.spi.ConnectionFactory;
 import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.Timeout;
@@ -32,26 +37,52 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import reactor.core.publisher.Mono;
 
-@Ignore
 @RunWith(JUnit4.class)
 public class R2dbcMssqlIntegrationTests {
 
-  @Rule
-  public Timeout globalTimeout= new Timeout(20, TimeUnit.SECONDS);
+  private static final ImmutableList<String> requiredEnvVars = ImmutableList
+      .of("MSSQL_USER", "MSSQL_PASS", "MSSQL_DB", "MSSQL_CONNECTION_NAME");
 
-  private ConnectionFactory connectionFactory;
+  private static final String CONNECTION_NAME = System.getenv("MSSQL_CONNECTION_NAME");
+  private static final String DB_NAME = System.getenv("MSSQL_DB");
+  private static final String DB_USER = System.getenv("MSSQL_USER");
+  private static final String DB_PASSWORD = System.getenv("MSSQL_PASS");
+
+  @Rule
+  public Timeout globalTimeout = new Timeout(20, TimeUnit.SECONDS);
+
+  private ConnectionPool connectionPool;
+  private String tableName;
 
   @Before
-  public void setUpConnection() {
-    this.connectionFactory =
-        ConnectionFactories.get(
-            "r2dbc:gcp:mssql:////user:password@connectionString/dbName");
+  public void setUpPool() {
+    // Check that required env vars are set
+    requiredEnvVars.forEach((varName) -> {
+      assertWithMessage(
+          String.format("Environment variable '%s' must be set to perform these tests.", varName))
+          .that(System.getenv(varName)).isNotEmpty();
+    });
 
-    Mono.from(this.connectionFactory.create())
+    // Set up URL parameters
+    String r2dbcURL = String
+        .format("r2dbc:gcp:mssql://%s:%s@%s/%s", DB_USER, DB_PASSWORD, CONNECTION_NAME,
+            DB_NAME);
+
+    // Initialize connection pool
+    ConnectionFactory connectionFactory = ConnectionFactories.get(r2dbcURL);
+    ConnectionPoolConfiguration configuration = ConnectionPoolConfiguration
+        .builder(connectionFactory)
+        .build();
+
+    this.connectionPool = new ConnectionPool(configuration);
+    this.tableName = String.format("books_%s", UUID.randomUUID().toString().replace("-", ""));
+
+    // Create table
+    Mono.from(this.connectionPool.create())
         .flatMapMany(
             c ->
                 c.createStatement(
-                    "CREATE TABLE books ("
+                    String.format("CREATE TABLE %s (", this.tableName)
                         + "  ID CHAR(20) NOT NULL,"
                         + "  TITLE TEXT NOT NULL"
                         + ")")
@@ -61,17 +92,19 @@ public class R2dbcMssqlIntegrationTests {
 
   @After
   public void dropTableIfPresent() {
-    Mono.from(this.connectionFactory.create())
-        .delayUntil(c -> c.createStatement("DROP TABLE books").execute())
+    String dropStmt = String.format("DROP TABLE %s", this.tableName);
+    Mono.from(this.connectionPool.create())
+        .delayUntil(c -> c.createStatement(dropStmt).execute())
         .block();
   }
 
   @Test
-  public void insertTest() {
-    Mono.from(this.connectionFactory.create())
+  public void pooledConnectionTest() {
+    String insertStmt = String.format("INSERT INTO %s (ID, TITLE) VALUES (@id, @title)", this.tableName);
+    Mono.from(this.connectionPool.create())
         .flatMapMany(
             c ->
-                c.createStatement("INSERT INTO books (ID, TITLE) VALUES (@id, @title)")
+                c.createStatement(insertStmt)
                     .bind("id", "book1")
                     .bind("title", "Book One")
                     .add()
@@ -81,14 +114,15 @@ public class R2dbcMssqlIntegrationTests {
         .flatMap(result -> result.map((row, rowMetadata) -> row.get(0)))
         .blockLast();
 
+    String selectStmt = String.format("SELECT TITLE FROM %s ORDER BY ID", this.tableName);
     List<String> books =
-        Mono.from(this.connectionFactory.create())
+        Mono.from(this.connectionPool.create())
             .flatMapMany(
                 connection ->
-                    connection.createStatement("SELECT TITLE FROM books ORDER BY ID").execute())
+                    connection.createStatement(selectStmt).execute())
             .flatMap(
-                spannerResult ->
-                    spannerResult.map(
+                result ->
+                    result.map(
                         (r, meta) -> r.get("TITLE", String.class)))
             .collectList()
             .block();
