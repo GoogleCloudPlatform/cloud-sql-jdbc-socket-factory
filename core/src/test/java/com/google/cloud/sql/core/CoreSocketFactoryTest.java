9cdf6de8cac309e81fe0b22db8c976921bc72870
Shubha Rajan <shubhadayini@google.com>
2021-07-21 14:36:29 -0700

    add Bouncy Castle provider

--- core/src/test/java/com/google/cloud/sql/core/CoreSocketFactoryTest.java
+++ core/src/test/java/com/google/cloud/sql/core/CoreSocketFactoryTest.java
@@ -69,8 +69,10 @@ import java.security.KeyStore;
 import java.security.KeyStore.PasswordProtection;
 import java.security.KeyStore.PrivateKeyEntry;
 import java.security.PrivateKey;
+import java.security.Provider;
 import java.security.PublicKey;
 import java.security.SecureRandom;
+import java.security.Security;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
@@ -92,8 +94,10 @@ import javax.net.ssl.SSLServerSocketFactory;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.TrustManagerFactory;
 import org.bouncycastle.asn1.x500.X500Name;
+import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
 import org.bouncycastle.cert.X509CertificateHolder;
 import org.bouncycastle.cert.X509v3CertificateBuilder;
+import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
 import org.bouncycastle.cert.jcajce.JcaX509v3CertificateBuilder;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.operator.ContentSigner;
@@ -462,24 +466,34 @@ public class CoreSocketFactoryTest {
         new PKCS8EncodedKeySpec(decodeBase64StripWhitespace(TestKeys.SIGNING_CA_PRIVATE_KEY));
     PrivateKey signingKey = keyFactory.generatePrivate(keySpec);
 
+    // add Bouncy Castle provider
+    Provider provider = new BouncyCastleProvider()
+    Security.addProvider(provider);
+
+    SubjectPublicKeyInfo subjectPublicKeyInfo = SubjectPublicKeyInfo.getInstance(
+        Futures.getDone(clientKeyPair).getPublic());
+
     final ContentSigner signer = new JcaContentSignerBuilder("SHA1withRSA")
         .setProvider(new BouncyCastleProvider())
         .build(signingKey);
 
-    X509v3CertificateBuilder certificateBuilder = new JcaX509v3CertificateBuilder(
+    X509v3CertificateBuilder certificateBuilder = new X509v3CertificateBuilder(
         new X500Name("C = US, O = Google\\, Inc, CN=temporary-subject"),
         BigInteger.ONE,
         Date.from(notBefore.toInstant()),
         Date.from(notAfter.toInstant()),
         new X500Name("C = US, O = Google\\, Inc, CN=Google Cloud SQL Signing CA foo:baz"),
-        Futures.getDone(clientKeyPair).getPublic()
+        subjectPublicKeyInfo
     );
 
     X509CertificateHolder certificateHolder = certificateBuilder.build(signer);
 
+    Certificate cert = new JcaX509CertificateConverter()
+        .getCertificate(certificateHolder);
+
     StringBuilder sb = new StringBuilder();
     sb.append("-----BEGIN CERTIFICATE-----\n");
-    sb.append(Base64.getEncoder().encodeToString(certificateHolder.getEncoded())
+    sb.append(Base64.getEncoder().encodeToString(cert.getEncoded())
         .replaceAll("(.{64})", "$1\n"));
     sb.append("\n");
     sb.append("-----END CERTIFICATE-----\n");
@@ -505,6 +519,7 @@ public class CoreSocketFactoryTest {
         @Override
         public void run() {
           try {
+
             CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
 
             KeyStore authKeyStore = KeyStore.getInstance(KeyStore.getDefaultType());
