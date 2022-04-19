ed9f717a5fa368e678c3ec788d2d31e40c54d757
Tomo Suzuki <suztomo@google.com>
2022-04-19 17:15:31 -0400

    debug-print access.findClassByName for CJException

--- core/src/main/java/com/google/cloud/sql/nativeimage/CloudSqlFeature.java
+++ core/src/main/java/com/google/cloud/sql/nativeimage/CloudSqlFeature.java
@@ -80,10 +80,12 @@ final class CloudSqlFeature implements Feature {
 
       NativeImageUtils.registerConstructorsForReflection(access, "com.mysql.cj.log.StandardLogger");
 
+      Class<?> klass = access.findClassByName("com.mysql.cj.exceptions.CJException");
+      System.out.println("access.findClassByName for com.mysql.cj.exceptions.CJException" + klass);
       access.registerSubtypeReachabilityHandler(
           (duringAccess, exceptionClass) ->
               NativeImageUtils.registerClassForReflection(duringAccess, exceptionClass.getName()),
-          access.findClassByName("com.mysql.cj.exceptions.CJException"));
+          klass);
 
       // JDBC classes create socket connections which must be initialized at run time.
       RuntimeClassInitialization.initializeAtRunTime("com.mysql.cj.jdbc");
