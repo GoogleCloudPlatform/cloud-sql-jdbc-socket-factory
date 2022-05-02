e915496544625c207adcadfe1d60d2ce19aeb02b
Shubha Rajan <shubhadayini@google.com>
2022-05-02 14:21:10 -0700

    add back refreshEnv calls

--- .kokoro/tests/run_tests_java17.bat
+++ .kokoro/tests/run_tests_java17.bat
@@ -1,3 +1,4 @@
 setx JAVA_HOME "C:\\OpenJDK\jdk-17"
 setx PATH "%JAVA_HOME%\bin;%PATH%"
+call RefreshEnv.cmd
 "C:\Program Files\Git\bin\bash.exe" github/cloud-sql-jdbc-socket-factory/.kokoro/tests/run_tests.sh
