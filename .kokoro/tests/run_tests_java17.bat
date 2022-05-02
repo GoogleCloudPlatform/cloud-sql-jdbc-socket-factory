1f5fc9d4ab62abbc449766a732f7eb6257fa6b40
Shubha Rajan <shubhadayini@google.com>
2022-05-02 12:47:14 -0700

    chore: fix path setting issue with Windows tests

--- .kokoro/tests/run_tests_java17.bat
+++ .kokoro/tests/run_tests_java17.bat
@@ -1,2 +1,3 @@
-set JAVA_HOME="C:\OpenJDK\jdk-17"
+setx JAVA_HOME "C:\\OpenJDK\jdk-17"
+setx PATH "%JAVA_HOME%\bin;%PATH%"
 "C:\Program Files\Git\bin\bash.exe" github/cloud-sql-jdbc-socket-factory/.kokoro/tests/run_tests.sh
