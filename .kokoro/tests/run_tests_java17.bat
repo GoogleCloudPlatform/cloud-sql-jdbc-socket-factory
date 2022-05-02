8fbc8ad5a5c722f8e5fed5e68076c49f867fd00c
Shubha Rajan <shubhadayini@google.com>
2022-05-02 14:31:47 -0700

    chore: fix path setting issue with Windows tests (#828)
    * chore: fix path setting issue with Windows tests
    
    * add back refreshEnv calls

--- .kokoro/tests/run_tests_java17.bat
+++ .kokoro/tests/run_tests_java17.bat
@@ -1,2 +1,4 @@
-set JAVA_HOME="C:\OpenJDK\jdk-17"
+setx JAVA_HOME "C:\\OpenJDK\jdk-17"
+setx PATH "%JAVA_HOME%\bin;%PATH%"
+call RefreshEnv.cmd
 "C:\Program Files\Git\bin\bash.exe" github/cloud-sql-jdbc-socket-factory/.kokoro/tests/run_tests.sh
