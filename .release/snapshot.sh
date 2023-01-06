477a4fe1cfe48733f66cc0410fbb9c5420f61333
Shubha Rajan <shubhadayini@google.com>
2023-01-06 12:12:11 -0800

    add snapshot script

---
+++ .release/snapshot.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+VERSION=$(cat version.txt)
+regex="([0-9]+).([0-9]+).([0-9]+)"
+
+if [[ $VERSION =~ $regex ]]; then
+  major="${BASH_REMATCH[1]}"
+  minor="${BASH_REMATCH[2]}"
+  build="${BASH_REMATCH[3]}"
+fi
+
+build="$(echo $build + 1 | bc)"
+
+echo "${major}.${minor}.${build}-SNAPSHOT" > version.txt
