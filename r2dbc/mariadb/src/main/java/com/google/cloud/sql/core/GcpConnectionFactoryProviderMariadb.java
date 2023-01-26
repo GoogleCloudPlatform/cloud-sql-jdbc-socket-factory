2c4459dbcea33d6d8b13e5c108388487f75dfec8
Shubha Rajan <shubhadayini@google.com>
2023-01-25 16:04:37 -0800

    fix license headers

--- r2dbc/mariadb/src/main/java/com/google/cloud/sql/core/GcpConnectionFactoryProviderMariadb.java
+++ r2dbc/mariadb/src/main/java/com/google/cloud/sql/core/GcpConnectionFactoryProviderMariadb.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 Google Inc. All Rights Reserved.
+ * Copyright 2023 Google LLC. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -39,7 +39,7 @@ public class GcpConnectionFactoryProviderMariadb extends GcpConnectionFactoryPro
   }
 
   @Override
-  ConnectionFactory tcpConnectonFactory(
+  ConnectionFactory tcpConnectionFactory(
       ConnectionFactoryOptions.Builder optionBuilder,
       Function<SslContextBuilder, SslContextBuilder> customizer,
       String csqlHostName) {
