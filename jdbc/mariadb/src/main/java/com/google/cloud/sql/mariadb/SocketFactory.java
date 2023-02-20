075be709f6299f7665d48e20471e4aacbd64b517
Farid Zakaria <fmzakari@google.com>
2023-02-20 23:21:03 +0000

    feat: Add MariaDB SocketFactory
    Add the implementation of a SocketFactory for
    use with MariaDB (or MySQL).
    Implementation largely resembles postgres.
    
    closes #1163

---
+++ jdbc/mariadb/src/main/java/com/google/cloud/sql/mariadb/SocketFactory.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.sql.mariadb;
+
+import com.google.cloud.sql.core.CoreSocketFactory;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.sql.SQLException;
+import org.mariadb.jdbc.Configuration;
+import org.mariadb.jdbc.util.ConfigurableSocketFactory;
+
+/**
+ * A MariaDB {@link SocketFactory} that establishes a secure connection to a Cloud SQL instance
+ * using ephemeral certificates.
+ *
+ * <p>The heavy lifting is done by the singleton {@link CoreSocketFactory} class.
+ */
+public class SocketFactory extends ConfigurableSocketFactory {
+
+  static {
+    CoreSocketFactory.addArtifactId("mariadb-socket-factory");
+  }
+
+  private Configuration conf;
+
+  public SocketFactory() throws SQLException {
+  }
+
+  @Override
+  public void setConfiguration(Configuration conf, String host) {
+    // Ignore the hostname
+    this.conf = conf;
+  }
+
+  @Override
+  public Socket createSocket() throws IOException {
+    try {
+      return CoreSocketFactory.connect(conf.nonMappedOptions());
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Override
+  public Socket createSocket(String s, int i) throws IOException, UnknownHostException {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public Socket createSocket(String s, int i, InetAddress inetAddress, int i1)
+      throws IOException, UnknownHostException {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public Socket createSocket(InetAddress inetAddress, int i) throws IOException {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public Socket createSocket(InetAddress inetAddress, int i, InetAddress inetAddress1, int i1)
+      throws IOException {
+    throw new UnsupportedOperationException();
+  }
+
+}
