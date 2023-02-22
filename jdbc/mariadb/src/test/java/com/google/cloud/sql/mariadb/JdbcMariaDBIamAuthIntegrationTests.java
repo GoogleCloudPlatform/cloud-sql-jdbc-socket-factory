c7bc5c8eed4e608499a14d26bc64da0b0ac0d5c0
Shubha Rajan <shubhadayini@google.com>
2023-02-22 10:35:49 -0800

    Update jdbc/mariadb/src/test/java/com/google/cloud/sql/mariadb/JdbcMariaDBIamAuthIntegrationTests.java

--- jdbc/mariadb/src/test/java/com/google/cloud/sql/mariadb/JdbcMariaDBIamAuthIntegrationTests.java
+++ jdbc/mariadb/src/test/java/com/google/cloud/sql/mariadb/JdbcMariaDBIamAuthIntegrationTests.java
@@ -44,7 +44,7 @@ import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
 public class JdbcMariaDBIamAuthIntegrationTests {
-  private static final String CONNECTION_NAME = System.getenv("MYSQL_CONNECTION_NAME");
+  private static final String CONNECTION_NAME = System.getenv("MYSQL_IAM_CONNECTION_NAME");
   private static final String DB_NAME = System.getenv("MYSQL_DB");
   private static final String DB_USER = System.getenv("MYSQL_IAM_USER");
   private static ImmutableList<String> requiredEnvVars = ImmutableList
