e8e0281464e0f0630ee6f3d0c0a0862c36a31d00
Shubha Rajan <shubhadayini@google.com>
2023-03-02 10:26:18 -0800

    chore(refactor): move application default credential factory to its own class

---
+++ core/src/main/java/com/google/cloud/sql/ApplicationDefaultCredentialFactory.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.sql;
+
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.services.sqladmin.SQLAdminScopes;
+import com.google.auth.http.HttpCredentialsAdapter;
+import com.google.auth.oauth2.GoogleCredentials;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class ApplicationDefaultCredentialFactory implements CredentialFactory {
+
+  @Override
+  public HttpRequestInitializer create() {
+    GoogleCredentials credentials;
+    try {
+      credentials = GoogleCredentials.getApplicationDefault();
+    } catch (IOException err) {
+      throw new RuntimeException(
+          "Unable to obtain credentials to communicate with the Cloud SQL API", err);
+    }
+    if (credentials.createScopedRequired()) {
+      credentials =
+          credentials.createScoped(Arrays.asList(
+              SQLAdminScopes.SQLSERVICE_ADMIN,
+              SQLAdminScopes.CLOUD_PLATFORM)
+          );
+    }
+    return new HttpCredentialsAdapter(credentials);
+  }
+}
--- core/src/main/java/com/google/cloud/sql/core/CoreSocketFactory.java
+++ core/src/main/java/com/google/cloud/sql/core/CoreSocketFactory.java
@@ -26,6 +26,7 @@ import com.google.api.services.sqladmin.SQLAdmin.Builder;
 import com.google.api.services.sqladmin.SQLAdminScopes;
 import com.google.auth.http.HttpCredentialsAdapter;
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.sql.ApplicationDefaultCredentialFactory;
 import com.google.cloud.sql.CredentialFactory;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Preconditions;
@@ -379,28 +380,6 @@ public final class CoreSocketFactory {
       adminApiBuilder.setServicePath(servicePath);
     }
     return adminApiBuilder.build();
-  }
-
-  private static class ApplicationDefaultCredentialFactory implements CredentialFactory {
-
-    @Override
-    public HttpRequestInitializer create() {
-      GoogleCredentials credentials;
-      try {
-        credentials = GoogleCredentials.getApplicationDefault();
-      } catch (IOException err) {
-        throw new RuntimeException(
-            "Unable to obtain credentials to communicate with the Cloud SQL API", err);
-      }
-      if (credentials.createScopedRequired()) {
-        credentials =
-            credentials.createScoped(Arrays.asList(
-                SQLAdminScopes.SQLSERVICE_ADMIN,
-                SQLAdminScopes.CLOUD_PLATFORM)
-            );
-      }
-      return new HttpCredentialsAdapter(credentials);
-    }
   }
 
   private static KeyPair generateRsaKeyPair() {
