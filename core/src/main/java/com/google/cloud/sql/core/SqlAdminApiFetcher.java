0285e3eb08f9e78adbc211f030b590f540a51dfc
Jonathan Hess <hessjc@google.com>
2023-06-14 11:02:20 -0600

    fix: only refresh if the token has expired.

--- core/src/main/java/com/google/cloud/sql/core/SqlAdminApiFetcher.java
+++ core/src/main/java/com/google/cloud/sql/core/SqlAdminApiFetcher.java
@@ -299,10 +299,17 @@ public class SqlAdminApiFetcher {
    * @throws IOException when the credentials.refresh() has failed 3 times
    */
   private void refreshWithRetry(OAuth2Credentials credentials) throws IOException {
+
+    // if the access token has not expired, do not attempt to refresh
+    AccessToken token = credentials.getAccessToken();
+    if( token == null || token.getExpirationTime().toInstant().isAfter(Instant.now()) ) {
+      return;
+    }
+
     Callable<OAuth2Credentials> refresh =
         () -> {
           try {
-            credentials.refresh();
+            credentials.refreshIfExpired();
           } catch (IllegalStateException e) {
             throw new IllegalStateException(
                 String.format(
