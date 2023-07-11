4474f16b986c0043c980f4f60dffc06801a63a16
Jonathan Hess (he/him) <103529393+hessjcg@users.noreply.github.com>
2023-07-11 14:44:42 -0600

    feat: Add support for PSC network connections (#1347)
    Adds new value for `ipTypes` jdbc connection parameter: `PSC`. When ipTypes=PSC, the connector will
    connect to the database instance using Private Service Connect instead of the instance's public or private
    IP address.

--- core/src/test/java/com/google/cloud/sql/core/SqlAdminApiFetcherTest.java
+++ core/src/test/java/com/google/cloud/sql/core/SqlAdminApiFetcherTest.java
@@ -39,6 +39,8 @@ public class SqlAdminApiFetcherTest {
 
   public static final String SAMPLE_PUBLIC_IP = "34.1.2.3";
   public static final String SAMPLE_PRIVATE_IP = "10.0.0.1";
+  public static final String SAMPLE_PSC_IP = "10.0.0.2";
+  public static final String SAMPLE_PCS_DNS_NAME = "abcde.12345.us-central1.sql.goog";
   public static final String INSTANCE_CONNECTION_NAME = "p:r:i";
   public static final String DATABASE_VERSION = "POSTGRES14";
 
@@ -63,6 +65,35 @@ public class SqlAdminApiFetcherTest {
     Map<String, String> ipAddrs = instanceData.getIpAddrs();
     assertThat(ipAddrs.get("PRIMARY")).isEqualTo(SAMPLE_PUBLIC_IP);
     assertThat(ipAddrs.get("PRIVATE")).isEqualTo(SAMPLE_PRIVATE_IP);
+    assertThat(ipAddrs.get("PSC")).isEqualTo(SAMPLE_PCS_DNS_NAME);
+  }
+
+  @Test
+  public void testFetchInstanceData_returnsPscForNonIpDatabase()
+      throws ExecutionException, InterruptedException, GeneralSecurityException,
+          OperatorCreationException {
+
+    MockAdminApi mockAdminApi = new MockAdminApi();
+    mockAdminApi.addConnectSettingsResponse(
+        INSTANCE_CONNECTION_NAME, null, null, DATABASE_VERSION, SAMPLE_PCS_DNS_NAME);
+    mockAdminApi.addGenerateEphemeralCertResponse(INSTANCE_CONNECTION_NAME, Duration.ofHours(1));
+
+    SqlAdminApiFetcher fetcher =
+        new StubApiFetcherFactory(mockAdminApi.getHttpTransport())
+            .create(new StubCredentialFactory().create());
+
+    InstanceData instanceData =
+        fetcher.getInstanceData(
+            new CloudSqlInstanceName(INSTANCE_CONNECTION_NAME),
+            () -> Optional.empty(),
+            AuthType.PASSWORD,
+            newTestExecutor(),
+            Futures.immediateFuture(mockAdminApi.getClientKeyPair()));
+    assertThat(instanceData.getSslContext()).isInstanceOf(SSLContext.class);
+
+    Map<String, String> ipAddrs = instanceData.getIpAddrs();
+    assertThat(ipAddrs.get("PSC")).isEqualTo(SAMPLE_PCS_DNS_NAME);
+    assertThat(ipAddrs.size()).isEqualTo(1);
   }
 
   private ListeningScheduledExecutorService newTestExecutor() {
@@ -129,7 +160,11 @@ public class SqlAdminApiFetcherTest {
       throws GeneralSecurityException, OperatorCreationException {
     MockAdminApi mockAdminApi = new MockAdminApi();
     mockAdminApi.addConnectSettingsResponse(
-        instanceConnectionName, SAMPLE_PUBLIC_IP, SAMPLE_PRIVATE_IP, databaseVersion);
+        instanceConnectionName,
+        SAMPLE_PUBLIC_IP,
+        SAMPLE_PRIVATE_IP,
+        databaseVersion,
+        SAMPLE_PCS_DNS_NAME);
     mockAdminApi.addGenerateEphemeralCertResponse(instanceConnectionName, Duration.ofHours(1));
     return mockAdminApi;
   }
