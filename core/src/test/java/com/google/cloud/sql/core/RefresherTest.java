2d41a07c99e3a4738c9ccc535c5e99631356367a
Jonathan Hess (he/him) <103529393+hessjcg@users.noreply.github.com>
2023-10-25 10:47:14 -0600

    test: Add RefresherTest to unit test the Refresher  (#1644)
    Adds unit tests to ensure that the Refresher logic is working.

---
+++ core/src/test/java/com/google/cloud/sql/core/RefresherTest.java
@@ -0,0 +1,407 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.sql.core;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertThrows;
+
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListeningScheduledExecutorService;
+import com.google.common.util.concurrent.MoreExecutors;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RefresherTest {
+
+  public static final long TEST_TIMEOUT_MS = 3000;
+
+  private AsyncRateLimiter rateLimiter = new AsyncRateLimiter(10);
+
+  private ListeningScheduledExecutorService executorService;
+
+  @Before
+  public void before() throws Exception {
+    executorService = newTestExecutor();
+  }
+
+  @After
+  public void after() {
+    executorService.shutdown();
+    executorService = null;
+  }
+
+  @Test
+  public void testCloudSqlInstanceDataRetrievedSuccessfully() {
+    ExampleData data = new ExampleData(Instant.now().plus(1, ChronoUnit.HOURS));
+    Refresher r =
+        new Refresher(
+            "testcase", executorService, () -> Futures.immediateFuture(data), rateLimiter);
+    InstanceData gotData = r.getData(TEST_TIMEOUT_MS);
+    assertThat(gotData).isSameInstanceAs(data);
+  }
+
+  @Test
+  public void testInstanceFailsOnConnectionError() {
+    Refresher r =
+        new Refresher(
+            "testcase",
+            executorService,
+            () -> Futures.immediateFailedFuture(new RuntimeException("always fails")),
+            rateLimiter);
+    RuntimeException ex = assertThrows(RuntimeException.class, () -> r.getData(TEST_TIMEOUT_MS));
+    assertThat(ex).hasMessageThat().contains("always fails");
+  }
+
+  @Test
+  public void testInstanceFailsOnTooLongToRetrieve() {
+    PauseCondition cond = new PauseCondition();
+    ExampleData data = new ExampleData(Instant.now().plus(1, ChronoUnit.HOURS));
+    Refresher r =
+        new Refresher(
+            "testcase",
+            executorService,
+            () -> {
+              cond.pause();
+              return Futures.immediateFuture(data);
+            },
+            rateLimiter);
+    RuntimeException ex = assertThrows(RuntimeException.class, () -> r.getData(TEST_TIMEOUT_MS));
+    assertThat(ex).hasMessageThat().contains("No refresh has completed");
+  }
+
+  @Test
+  public void testCloudSqlInstanceForcesRefresh() throws Exception {
+    ExampleData data = new ExampleData(Instant.now().plus(1, ChronoUnit.HOURS));
+    AtomicInteger refreshCount = new AtomicInteger();
+    final PauseCondition cond = new PauseCondition();
+    Refresher r =
+        new Refresher(
+            "testcase",
+            executorService,
+            () -> {
+              int c = refreshCount.get();
+              // Allow the first execution to complete immediately.
+              // The second execution should pause until signaled.
+              if (c == 1) {
+                cond.pause();
+              }
+              refreshCount.incrementAndGet();
+              return Futures.immediateFuture(data);
+            },
+            rateLimiter);
+    r.getData(TEST_TIMEOUT_MS);
+    assertThat(refreshCount.get()).isEqualTo(1);
+
+    // Force refresh, which will start, but not finish the refresh process.
+    r.forceRefresh();
+
+    // Then immediately getSslData() and assert that the refresh count has not changed.
+    // Refresh count hasn't changed because we re-use the existing connection info.
+    r.getData(TEST_TIMEOUT_MS);
+    assertThat(refreshCount.get()).isEqualTo(1);
+
+    // Allow the second refresh operation to complete
+    cond.proceed();
+    cond.waitForPauseToEnd(1000L);
+    cond.waitForCondition(() -> refreshCount.get() >= 2, 1000L);
+
+    // getSslData again, and assert the refresh operation completed.
+    r.getData(TEST_TIMEOUT_MS);
+    assertThat(refreshCount.get()).isEqualTo(2);
+  }
+
+  @Test
+  public void testCloudSqlInstanceRetriesOnInitialFailures() throws Exception {
+    ExampleData data = new ExampleData(Instant.now().plus(1, ChronoUnit.HOURS));
+
+    AtomicInteger refreshCount = new AtomicInteger();
+
+    Refresher r =
+        new Refresher(
+            "testcase",
+            executorService,
+            () -> {
+              int c = refreshCount.get();
+              refreshCount.incrementAndGet();
+              if (c == 0) {
+                throw new RuntimeException("bad request 0");
+              }
+              return Futures.immediateFuture(data);
+            },
+            rateLimiter);
+
+    // Get the first data that is about to expire
+    long until = System.currentTimeMillis() + 3000;
+    while (r.getData(TEST_TIMEOUT_MS) != data && System.currentTimeMillis() < until) {
+      Thread.sleep(100);
+    }
+    assertThat(refreshCount.get()).isEqualTo(2);
+    assertThat(r.getData(TEST_TIMEOUT_MS)).isEqualTo(data);
+  }
+
+  @Test
+  public void testCloudSqlRefreshesExpiredData() throws Exception {
+    ExampleData initialData = new ExampleData(Instant.now().plus(2, ChronoUnit.SECONDS));
+    ExampleData data = new ExampleData(Instant.now().plus(1, ChronoUnit.HOURS));
+
+    AtomicInteger refreshCount = new AtomicInteger();
+    final PauseCondition refresh1 = new PauseCondition();
+
+    Refresher r =
+        new Refresher(
+            "testcase",
+            executorService,
+            () -> {
+              int c = refreshCount.get();
+              ExampleData refreshResult = data;
+              switch (c) {
+                case 0:
+                  // refresh 0 should return initialData immediately
+                  refreshResult = initialData;
+                  break;
+                case 1:
+                  // refresh 1 should pause
+                  refresh1.pause();
+                  break;
+              }
+              // refresh 2 and on should return data immediately
+              refreshCount.incrementAndGet();
+              return Futures.immediateFuture(refreshResult);
+            },
+            rateLimiter);
+
+    // Get the first data that is about to expire
+    InstanceData d = r.getData(TEST_TIMEOUT_MS);
+    assertThat(refreshCount.get()).isEqualTo(1);
+    assertThat(d).isSameInstanceAs(r.getData(TEST_TIMEOUT_MS));
+
+    // Wait for the instance to expire
+    while (Instant.now().isBefore(initialData.getExpiration())) {
+      Thread.sleep(10);
+    }
+
+    // Allow the second refresh operation to complete
+    refresh1.proceed();
+    refresh1.waitForPauseToEnd(1000L);
+
+    // getSslData again, and assert the refresh operation completed.
+    refresh1.waitForCondition(() -> r.getData(TEST_TIMEOUT_MS) == data, 1000L);
+  }
+
+  @Test
+  public void testThatForceRefreshBalksWhenAScheduledRefreshIsInProgress() throws Exception {
+    // Set expiration 1 minute in the future, so that it will trigger a scheduled refresh
+    // and won't expire during this testcase.
+    ExampleData expiresInOneMinute = new ExampleData(Instant.now().plus(1, ChronoUnit.MINUTES));
+
+    // Set the next refresh data expiration 1 hour in the future.
+    ExampleData data = new ExampleData(Instant.now().plus(1, ChronoUnit.HOURS));
+
+    AtomicInteger refreshCount = new AtomicInteger();
+    final PauseCondition refresh0 = new PauseCondition();
+    final PauseCondition refresh1 = new PauseCondition();
+
+    Refresher r =
+        new Refresher(
+            "testcase",
+            executorService,
+            () -> {
+              int c = refreshCount.get();
+              ExampleData refreshResult = data;
+              switch (c) {
+                case 0:
+                  refresh0.pause();
+                  refreshResult = expiresInOneMinute;
+                  break;
+                case 1:
+                  refresh1.pause();
+                  break;
+              }
+              refreshCount.incrementAndGet();
+              return Futures.immediateFuture(refreshResult);
+            },
+            rateLimiter);
+
+    refresh0.proceed();
+    refresh0.waitForPauseToEnd(1000);
+    refresh0.waitForCondition(() -> refreshCount.get() > 0, 1000);
+    // Get the first data that is about to expire
+    assertThat(refreshCount.get()).isEqualTo(1);
+    InstanceData d = r.getData(TEST_TIMEOUT_MS);
+    assertThat(d).isSameInstanceAs(expiresInOneMinute);
+
+    // Because the data is about to expire, scheduled refresh will begin immediately.
+    // Wait until refresh is in progress.
+    refresh1.waitForPauseToStart(1000);
+
+    // Then call forceRefresh(), which should balk because a refresh attempt is in progress.
+    r.forceRefresh();
+
+    // Finally, allow the scheduled refresh operation to complete
+    refresh1.proceed();
+    refresh1.waitForPauseToEnd(5000);
+    refresh1.waitForCondition(() -> refreshCount.get() > 1, 1000);
+
+    // Now that the InstanceData has expired, this getSslData should pause until new data
+    // has been retrieved.
+
+    // getSslData again, and assert the refresh operation completed.
+    refresh1.waitForCondition(() -> r.getData(TEST_TIMEOUT_MS) == data, 1000L);
+    assertThat(refreshCount.get()).isEqualTo(2);
+  }
+
+  @Test
+  public void testThatForceRefreshBalksWhenAForceRefreshIsInProgress() throws Exception {
+    ExampleData initialData = new ExampleData(Instant.now().plus(1, ChronoUnit.HOURS));
+    ExampleData data = new ExampleData(Instant.now().plus(1, ChronoUnit.HOURS));
+
+    AtomicInteger refreshCount = new AtomicInteger();
+    final PauseCondition refresh1 = new PauseCondition();
+
+    Refresher r =
+        new Refresher(
+            "testcase",
+            executorService,
+            () -> {
+              int c = refreshCount.get();
+              switch (c) {
+                case 0:
+                  refreshCount.incrementAndGet();
+                  return Futures.immediateFuture(initialData);
+                case 1:
+                  refresh1.pause();
+                  refreshCount.incrementAndGet();
+                  return Futures.immediateFuture(data);
+                default:
+                  return Futures.immediateFuture(data);
+              }
+            },
+            rateLimiter);
+
+    // Get the first data that is about to expire
+    InstanceData d = r.getData(TEST_TIMEOUT_MS);
+    assertThat(refreshCount.get()).isEqualTo(1);
+    assertThat(d).isSameInstanceAs(initialData);
+
+    // call forceRefresh twice, this should only result in 1 refresh fetch
+    r.forceRefresh();
+    r.forceRefresh();
+
+    // Allow the refresh operation to complete
+    refresh1.proceed();
+
+    // Now that the InstanceData has expired, this getSslData should pause until new data
+    // has been retrieved.
+    refresh1.waitForPauseToEnd(1000);
+    refresh1.waitForCondition(() -> refreshCount.get() >= 2, 1000);
+
+    // assert the refresh operation completed exactly once after
+    // forceRefresh was called multiple times.
+    refresh1.waitForCondition(() -> r.getData(TEST_TIMEOUT_MS) == data, 1000L);
+    assertThat(refreshCount.get()).isEqualTo(2);
+  }
+
+  @Test
+  public void testRefreshRetriesOnAfterFailedAttempts() throws Exception {
+    ExampleData aboutToExpireData = new ExampleData(Instant.now().plus(10, ChronoUnit.MILLIS));
+    ExampleData data = new ExampleData(Instant.now().plus(1, ChronoUnit.HOURS));
+
+    AtomicInteger refreshCount = new AtomicInteger();
+    final PauseCondition badRequest1 = new PauseCondition();
+    final PauseCondition badRequest2 = new PauseCondition();
+    final PauseCondition goodRequest = new PauseCondition();
+
+    Refresher r =
+        new Refresher(
+            "testcase",
+            executorService,
+            () -> {
+              int c = refreshCount.get();
+              switch (c) {
+                case 0:
+                  refreshCount.incrementAndGet();
+                  return Futures.immediateFuture(aboutToExpireData);
+                case 1:
+                  badRequest1.pause();
+                  refreshCount.incrementAndGet();
+                  throw new RuntimeException("bad request 1");
+                case 2:
+                  badRequest2.pause();
+                  refreshCount.incrementAndGet();
+                  throw new RuntimeException("bad request 2");
+                default:
+                  goodRequest.pause();
+                  refreshCount.incrementAndGet();
+                  return Futures.immediateFuture(data);
+              }
+            },
+            rateLimiter);
+
+    // Get the first data that is about to expire
+    InstanceData d = r.getData(TEST_TIMEOUT_MS);
+    assertThat(refreshCount.get()).isEqualTo(1);
+    assertThat(d).isSameInstanceAs(aboutToExpireData);
+
+    // Don't force a refresh, this should automatically schedule a refresh right away because
+    // the token returned in the first request had less than 4 minutes before it expired.
+
+    // Wait for the current InstanceData to actually expire.
+    while (Instant.now().isBefore(aboutToExpireData.getExpiration())) {
+      Thread.sleep(10);
+    }
+
+    // Orchestrate the failed attempts
+
+    // Allow the second refresh operation to complete
+    badRequest1.proceed();
+    badRequest1.waitForPauseToEnd(5000);
+    badRequest1.waitForCondition(() -> refreshCount.get() == 2, 2000);
+
+    // Allow the second bad request completes
+    badRequest2.proceed();
+    badRequest2.waitForCondition(() -> refreshCount.get() == 3, 2000);
+
+    // Allow the final good request to complete
+    goodRequest.proceed();
+    goodRequest.waitForCondition(() -> refreshCount.get() == 4, 2000);
+
+    // Try getSslData() again, and assert the refresh operation eventually completes.
+    goodRequest.waitForCondition(() -> r.getData(TEST_TIMEOUT_MS) == data, 2000);
+  }
+
+  private static class ExampleData extends InstanceData {
+
+    ExampleData(Instant expiration) {
+      super(new Metadata(null, null), new SslData(null, null, null), expiration);
+    }
+  }
+
+  private ListeningScheduledExecutorService newTestExecutor() {
+    ScheduledThreadPoolExecutor executor =
+        (ScheduledThreadPoolExecutor) Executors.newScheduledThreadPool(2);
+    executor.setExecuteExistingDelayedTasksAfterShutdownPolicy(false);
+    executor.setContinueExistingPeriodicTasksAfterShutdownPolicy(false);
+    return MoreExecutors.listeningDecorator(
+        MoreExecutors.getExitingScheduledExecutorService(executor));
+  }
+}
