2c8ae93b49c65bb0ed907f1ee7f49d69c91e6388
Jonathan Hess (he/him) <103529393+hessjcg@users.noreply.github.com>
2024-05-15 09:37:08 -0600

    chore: Add LazyRefreshStrategy implementing the lazy refresh straetgy (part of #992) (#1970)
    Implementation and unit test for the LazyRefreshStrategy.
    
    Part of #992

---
+++ core/src/main/java/com/google/cloud/sql/core/LazyRefreshStrategy.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.sql.core;
+
+import com.google.errorprone.annotations.concurrent.GuardedBy;
+import java.time.Instant;
+import java.util.function.Supplier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/** RefreshStrategy that implements the lazy refresh strategy. */
+public class LazyRefreshStrategy implements RefreshStrategy {
+  private final Logger logger = LoggerFactory.getLogger(LazyRefreshStrategy.class);
+
+  private final String name;
+  private final Supplier<ConnectionInfo> refreshOperation;
+
+  private final Object connectionInfoGuard = new Object();
+
+  @GuardedBy("connectionInfoGuard")
+  private ConnectionInfo connectionInfo;
+
+  @GuardedBy("connectionInfoGuard")
+  private boolean closed;
+
+  /** Creates a new LazyRefreshStrategy instance. */
+  public LazyRefreshStrategy(String name, Supplier<ConnectionInfo> refreshOperation) {
+    this.name = name;
+    this.refreshOperation = refreshOperation;
+  }
+
+  @Override
+  public ConnectionInfo getConnectionInfo(long timeoutMs) {
+    synchronized (connectionInfoGuard) {
+      if (closed) {
+        throw new IllegalStateException(
+            String.format("[%s] Lazy Refresh: Named connection closed.", name));
+      }
+
+      if (connectionInfo == null) {
+        logger.debug(
+            String.format(
+                "[%s] Lazy Refresh Operation: No client certificate. Starting next refresh "
+                    + "operation immediately.",
+                name));
+        fetchConnectionInfo();
+      }
+      if (Instant.now().isAfter(connectionInfo.getExpiration())) {
+        logger.debug(
+            String.format(
+                "[%s] Lazy Refresh Operation: Client certificate has expired. Starting next "
+                    + "refresh operation immediately.",
+                name));
+        fetchConnectionInfo();
+      }
+      return connectionInfo;
+    }
+  }
+
+  private void fetchConnectionInfo() {
+    synchronized (connectionInfoGuard) {
+      logger.debug(String.format("[%s] Lazy Refresh Operation: Starting refresh operation.", name));
+      try {
+        this.connectionInfo = this.refreshOperation.get();
+      } catch (TerminalException e) {
+        logger.debug(String.format("[%s] Lazy Refresh Operation: Failed! No retry.", name), e);
+        throw e;
+      } catch (Exception e) {
+        throw new RuntimeException(String.format("[%s] Refresh Operation: Failed!", name), e);
+      }
+
+      logger.debug(
+          String.format(
+              "[%s] Lazy Refresh Operation: Completed refresh with new certificate "
+                  + "expiration at %s.",
+              name, connectionInfo.getExpiration().toString()));
+    }
+  }
+
+  /** Force a new refresh of the instance data if the client certificate has expired. */
+  @Override
+  public void forceRefresh() {
+    // invalidate connectionInfo so that the next call to getConectionInfo() will
+    // fetch new data.
+    synchronized (connectionInfoGuard) {
+      if (closed) {
+        throw new IllegalStateException(
+            String.format("[%s] Lazy Refresh: Named connection closed.", name));
+      }
+      this.connectionInfo = null;
+      logger.debug(String.format("[%s] Lazy Refresh Operation: Forced refresh.", name));
+    }
+  }
+
+  /** Force a new refresh of the instance data if the client certificate has expired. */
+  @Override
+  public void refreshIfExpired() {
+    synchronized (connectionInfoGuard) {
+      if (closed) {
+        throw new IllegalStateException(
+            String.format("[%s] Lazy Refresh: Named connection closed.", name));
+      }
+    }
+  }
+
+  @Override
+  public void close() {
+    synchronized (connectionInfoGuard) {
+      closed = true;
+      logger.debug(String.format("[%s] Lazy Refresh Operation: Connector closed.", name));
+    }
+  }
+}
---
+++ core/src/test/java/com/google/cloud/sql/core/LazyRefreshStrategyTest.java
@@ -0,0 +1,181 @@
+/*
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.sql.core;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertThrows;
+
+import com.google.common.util.concurrent.RateLimiter;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.junit.Test;
+
+public class LazyRefreshStrategyTest {
+  public static final long TEST_TIMEOUT_MS = 3000;
+  final double delayBetweenAttemptSeconds = 0.25;
+  final RateLimiter rateLimiter = RateLimiter.create(1.0 / delayBetweenAttemptSeconds);
+
+  @Test
+  public void testCloudSqlInstanceDataRetrievedSuccessfully() {
+    final ExampleData data = new ExampleData(Instant.now().plus(1, ChronoUnit.HOURS));
+    LazyRefreshStrategy r =
+        new LazyRefreshStrategy(
+            "LazyRefresherTest.testCloudSqlInstanceDataRetrievedSuccessfully", () -> data);
+    ConnectionInfo gotInfo = r.getConnectionInfo(TEST_TIMEOUT_MS);
+    assertThat(gotInfo).isSameInstanceAs(data);
+  }
+
+  @Test
+  public void testInstanceFailsOnConnectionError() {
+    LazyRefreshStrategy r =
+        new LazyRefreshStrategy(
+            "LazyRefresherTest.testInstanceFailsOnConnectionError",
+            () -> {
+              throw new RuntimeException("always fails");
+            });
+
+    RuntimeException ex =
+        assertThrows(RuntimeException.class, () -> r.getConnectionInfo(TEST_TIMEOUT_MS));
+
+    assertThat(ex.getCause()).hasMessageThat().contains("always fails");
+  }
+
+  @Test
+  public void testCloudSqlInstanceForcesRefresh() throws Exception {
+    ExampleData data = new ExampleData(Instant.now().plus(1, ChronoUnit.HOURS));
+    AtomicInteger refreshCount = new AtomicInteger();
+    LazyRefreshStrategy r =
+        new LazyRefreshStrategy(
+            "LazyRefresherTest.testCloudSqlInstanceForcesRefresh",
+            () -> {
+              refreshCount.incrementAndGet();
+              return data;
+            });
+
+    r.getConnectionInfo(TEST_TIMEOUT_MS);
+    assertThat(refreshCount.get()).isEqualTo(1);
+
+    // Force refresh, which will start, but not finish the refresh process.
+    r.forceRefresh();
+
+    // Then immediately getSslData() and assert that the refresh count has not changed.
+    // Refresh count hasn't changed because we re-use the existing connection info.
+    r.getConnectionInfo(TEST_TIMEOUT_MS);
+    assertThat(refreshCount.get()).isEqualTo(2);
+
+    // getSslData again, and assert the refresh operation completed.
+    r.getConnectionInfo(TEST_TIMEOUT_MS);
+    assertThat(refreshCount.get()).isEqualTo(2);
+  }
+
+  @Test
+  public void testCloudSqlRefreshesExpiredData() throws Exception {
+    ExampleData initialData = new ExampleData(Instant.now().plus(2, ChronoUnit.SECONDS));
+    ExampleData data = new ExampleData(Instant.now().plus(1, ChronoUnit.HOURS));
+
+    AtomicInteger refreshCount = new AtomicInteger();
+
+    LazyRefreshStrategy r =
+        new LazyRefreshStrategy(
+            "LazyRefresherTest.testCloudSqlRefreshesExpiredData",
+            () -> {
+              int c = refreshCount.getAndIncrement();
+              if (c == 0) {
+                return initialData;
+              }
+              return data;
+            });
+
+    // Get the first data that is about to expire
+    ConnectionInfo d = r.getConnectionInfo(TEST_TIMEOUT_MS);
+    assertThat(refreshCount.get()).isEqualTo(1);
+    assertThat(d).isSameInstanceAs(initialData);
+
+    // Wait for the instance to expire
+    while (Instant.now().isBefore(initialData.getExpiration())) {
+      Thread.sleep(10);
+    }
+
+    assertThat(r.getConnectionInfo(TEST_TIMEOUT_MS)).isSameInstanceAs(data);
+    assertThat(refreshCount.get()).isEqualTo(2);
+  }
+
+  @Test
+  public void testThatConcurrentRequestsDontCauseDuplicateRefreshAttempts() throws Exception {
+    ExampleData initialData = new ExampleData(Instant.now().plus(2, ChronoUnit.SECONDS));
+    ExampleData data = new ExampleData(Instant.now().plus(1, ChronoUnit.HOURS));
+
+    AtomicInteger refreshCount = new AtomicInteger();
+
+    LazyRefreshStrategy r =
+        new LazyRefreshStrategy(
+            "LazyRefresherTest.testThatConcurrentRequestsDontCauseDuplicateRefreshAttempts",
+            () -> {
+              int c = refreshCount.getAndIncrement();
+              if (c == 0) {
+                return initialData;
+              }
+              return data;
+            });
+
+    // Get the first data that is about to expire
+    ConnectionInfo d = r.getConnectionInfo(TEST_TIMEOUT_MS);
+    assertThat(refreshCount.get()).isEqualTo(1);
+    assertThat(d).isSameInstanceAs(initialData);
+
+    // Wait for the instance to expire
+    while (Instant.now().isBefore(initialData.getExpiration())) {
+      Thread.sleep(10);
+    }
+    // Introduce a little more delay to allow for Java 8
+    Thread.sleep(100);
+
+    // Start multiple threads and request connection info
+    Thread t1 = new Thread(() -> r.getConnectionInfo(TEST_TIMEOUT_MS));
+    Thread t2 = new Thread(() -> r.getConnectionInfo(TEST_TIMEOUT_MS));
+    Thread t3 = new Thread(() -> r.getConnectionInfo(TEST_TIMEOUT_MS));
+    t1.start();
+    t2.start();
+    t3.start();
+    t1.join();
+    t2.join();
+    t3.join();
+
+    // Assert that only one more refresh operation was performed.
+    assertThat(refreshCount.get()).isEqualTo(2);
+    assertThat(r.getConnectionInfo(TEST_TIMEOUT_MS)).isSameInstanceAs(data);
+  }
+
+  @Test
+  public void testClosedCloudSqlInstanceDataThrowsException() {
+    ExampleData data = new ExampleData(Instant.now().plus(1, ChronoUnit.HOURS));
+    LazyRefreshStrategy r =
+        new LazyRefreshStrategy(
+            "RefresherTest.testClosedCloudSqlInstanceDataThrowsException", () -> data);
+    r.close();
+
+    assertThrows(IllegalStateException.class, () -> r.getConnectionInfo(TEST_TIMEOUT_MS));
+    assertThrows(IllegalStateException.class, () -> r.forceRefresh());
+  }
+
+  private static class ExampleData extends ConnectionInfo {
+    ExampleData(Instant expiration) {
+      super(new InstanceMetadata(null, null), new SslData(null, null, null), expiration);
+    }
+  }
+}
