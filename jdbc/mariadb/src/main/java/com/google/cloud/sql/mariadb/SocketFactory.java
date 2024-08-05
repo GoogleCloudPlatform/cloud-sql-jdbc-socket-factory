b36841640c9d60503abf73eb255b15183a04aa6d
Jonathan Hess <hessjc@google.com>
2024-08-05 13:28:52 -0600

    wip: Use domain name from JDBC URL for Postgres and Mysql

--- jdbc/mariadb/src/main/java/com/google/cloud/sql/mariadb/SocketFactory.java
+++ jdbc/mariadb/src/main/java/com/google/cloud/sql/mariadb/SocketFactory.java
@@ -37,6 +37,7 @@ public class SocketFactory extends ConfigurableSocketFactory {
   }
 
   private Configuration conf;
+  private String host;
 
   public SocketFactory() {}
 
@@ -44,13 +45,14 @@ public class SocketFactory extends ConfigurableSocketFactory {
   public void setConfiguration(Configuration conf, String host) {
     // Ignore the hostname
     this.conf = conf;
+    this.host = host;
   }
 
   @Override
   public Socket createSocket() throws IOException {
     try {
       return InternalConnectorRegistry.getInstance()
-          .connect(ConnectionConfig.fromConnectionProperties(conf.nonMappedOptions()));
+          .connect(ConnectionConfig.fromConnectionProperties(conf.nonMappedOptions(), host));
     } catch (InterruptedException e) {
       throw new RuntimeException(e);
     }
