d6867d987222c9c3d0ad91fe257d4864f4dc5274
Jonathan Hess <hessjc@google.com>
2025-09-18 09:53:17 -0600

    chore: Fix environment variable secret names in build.sh to match the e2e test source code.

--- build.sh
+++ build.sh
@@ -84,21 +84,21 @@ function deps() {
 
 # write_e2e_env - Loads secrets from the gcloud project and writes
 #     them to target/e2e.env to run e2e tests.
-#
 function write_e2e_env(){
   # Set the default to .envrc file if no argument is passed
   outfile="${1:-.envrc}"
   secret_vars=(
     MYSQL_CONNECTION_NAME=MYSQL_CONNECTION_NAME
     MYSQL_USER=MYSQL_USER
-    MYSQL_USER_IAM=MYSQL_USER_IAM_GO
+    MYSQL_USER_IAM=MYSQL_USER_IAM_JAVA
     MYSQL_PASS=MYSQL_PASS
     MYSQL_DB=MYSQL_DB
     MYSQL_MCP_CONNECTION_NAME=MYSQL_MCP_CONNECTION_NAME
     MYSQL_MCP_PASS=MYSQL_MCP_PASS
     POSTGRES_CONNECTION_NAME=POSTGRES_CONNECTION_NAME
+    POSTGRES_IAM_CONNECTION_NAME=POSTGRES_IAM_CONNECTION_NAME
     POSTGRES_USER=POSTGRES_USER
-    POSTGRES_USER_IAM=POSTGRES_USER_IAM_GO
+    POSTGRES_USER_IAM=POSTGRES_USER_IAM_JAVA
     POSTGRES_PASS=POSTGRES_PASS
     POSTGRES_DB=POSTGRES_DB
     POSTGRES_CAS_CONNECTION_NAME=POSTGRES_CAS_CONNECTION_NAME
@@ -113,10 +113,11 @@ function write_e2e_env(){
     SQLSERVER_USER=SQLSERVER_USER
     SQLSERVER_PASS=SQLSERVER_PASS
     SQLSERVER_DB=SQLSERVER_DB
+    IMPERSONATED_USER=IMPERSONATED_USER
     QUOTA_PROJECT=QUOTA_PROJECT
   )
 
-  if [[ -z "${TEST_PROJECT:-}" ]] ; then
+  if [[ -z "$TEST_PROJECT" ]] ; then
     echo "Set TEST_PROJECT environment variable to the project containing"
     echo "the e2e test suite secrets."
     exit 1
