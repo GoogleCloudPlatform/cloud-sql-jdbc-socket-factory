2004209b60d4172c2e28defbbac4a07bf0e8e4d1
Jonathan Hess <hessjc@google.com>
2025-09-22 15:19:27 -0600

    chore: fix lint issues.

--- jdbc/mysql-j-8/src/main/java/com/google/cloud/sql/mysql/SocketFactory.java
+++ jdbc/mysql-j-8/src/main/java/com/google/cloud/sql/mysql/SocketFactory.java
@@ -60,7 +60,8 @@ public class SocketFactory implements com.mysql.cj.protocol.SocketFactory {
     T socket =
         (T)
             InternalConnectorRegistry.getInstance()
-                .connect(ConnectionConfig.fromConnectionProperties(props, host)
+                .connect(
+                    ConnectionConfig.fromConnectionProperties(props, host)
                         .withMdxClientProtocolType("tls"));
     return socket;
   }
