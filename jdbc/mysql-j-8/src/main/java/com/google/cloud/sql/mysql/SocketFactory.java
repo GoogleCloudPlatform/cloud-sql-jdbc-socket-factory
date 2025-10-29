87b3765c125a278c55316874df9e9903fae62b81
Jonathan Hess (he/him) <103529393+hessjcg@users.noreply.github.com>
2025-10-29 14:20:28 -0600

    feat: Automatically set MDX client_protocol_type=TLS for MySQL connections. (#2207)
    * feat: Automatically set MDX client_protocol_type=TLS for MySQL connections.
    
    When a Cloud SQL instance supports Metadata Exchange, the MySQL socket factory will 
    automatically set MDX client_protocol_type=TLS. This will ensure that the password
    auth protocol works correctly. 
    See also #2200 and GoogleCloudPlatform/cloud-sql-proxy#2317

--- jdbc/mysql-j-8/src/main/java/com/google/cloud/sql/mysql/SocketFactory.java
+++ jdbc/mysql-j-8/src/main/java/com/google/cloud/sql/mysql/SocketFactory.java
@@ -60,10 +60,9 @@ public class SocketFactory implements com.mysql.cj.protocol.SocketFactory {
     T socket =
         (T)
             InternalConnectorRegistry.getInstance()
-                .connect(ConnectionConfig.fromConnectionProperties(props, host));
-    // TODO: When MDX is fully supported by the server,
-    //      add this configuration to automatically set the client protocol type to TLS.
-    //      .withMdxClientProtocolType("tls"));
+                .connect(
+                    ConnectionConfig.fromConnectionProperties(props, host)
+                        .withMdxClientProtocolType("tls"));
     return socket;
   }
 
